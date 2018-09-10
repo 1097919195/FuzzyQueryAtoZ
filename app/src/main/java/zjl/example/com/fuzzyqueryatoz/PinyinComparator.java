87891817ae9ebb6b2248d6da71be653def39952c
zjl7577 <1097919195@qq.com>
2018-09-10 16:49:30 +0800

    first

---
+++ app/src/main/java/zjl/example/com/fuzzyqueryatoz/PinyinComparator.java
@@ -0,0 +1,24 @@
+package zjl.example.com.fuzzyqueryatoz;
+
+import java.util.Comparator;
+
+/**
+ * 
+ * @author xiaanming
+ *
+ */
+public class PinyinComparator implements Comparator<SortModel> {
+
+
+	@Override
+	public int compare(SortModel o1, SortModel o2) {
+		if (o1.sortLetters.equals("@") || o2.sortLetters.equals("#")) {
+			return -1;
+		} else if (o1.sortLetters.equals("#") || o2.sortLetters.equals("@")) {
+			return 1;
+		} else {
+			return o1.sortLetters.compareTo(o2.sortLetters);
+		}
+	}
+
+}
